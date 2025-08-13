594fb1ad7a79447152e43b0accbaccf3ed95ee51
nov <novslog@gmail.com>
2025-08-13 21:34:58 +0900

    fix : Spring backend Migration

---
+++ backend/src/main/java/com/novs/ragBlog/RagBlogApplication.java
@@ -0,0 +1,13 @@
+package com.novs.ragBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagBlogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/novs/ragBlog/RagBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novs.ragBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
